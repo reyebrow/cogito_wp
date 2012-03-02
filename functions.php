79270df95bda2ff5cbfa8abd3ff9804d63520348
Matt <matt@raisedeyebrow.com>
2012-03-02 09:34:14 -0800

    fix so that child themes can override column sizes

--- functions.php
+++ functions.php
@@ -25,34 +25,36 @@ file
 
 ALL SECTIONS MUST ADD UP TO 12 (or not. maybe you like the look of a broken site)
 *********************************************************************************/
-$cogito_init = array(
-  //Three columns with right and left sidebar
-  'three_columns_left'     => 3,
-  'three_columns_content'  => 6,
-  'three_columns_right'    => 3,
+if (!isset($cogito_init) ){
+  $cogito_init = array(
+    //Three columns with right and left sidebar
+    'three_columns_left'     => 3,
+    'three_columns_content'  => 6,
+    'three_columns_right'    => 3,
+    
+    //Two columns with right sidebar
+    'two_columns_rsb_right'   => 4,
+    'two_columns_rsb_content' => 8,
+    
+    //Two columns with Left sidebar
+    'two_columns_lsb_left'    => 3,
+    'two_columns_lsb_content' => 9,
+    
+    //1 Column. Centered by default
+    'one_column_content'      => 10,
   
-  //Two columns with right sidebar
-  'two_columns_rsb_right'   => 4,
-  'two_columns_rsb_content' => 8,
-  
-  //Two columns with Left sidebar
-  'two_columns_lsb_left'    => 3,
-  'two_columns_lsb_content' => 9,
-  
-  //1 Column. Centered by default
-  'one_column_content'      => 10,
-
-/******************************************************************************************************
- *  Sets an array corresponding to the number and widths of your footers from left to right. 
- *  You can have as many footers as you want but the widths MUST add up to 12 or Foundation columns 
- *  will hate you and stop answering your phone calls.
- * 
- *  Example:  3 columns of equal width  array(4,4,4);
-              2 columns of widths 4 and 8 array(4,8);
- *****************************************************************************************************/
-  
-  'footers' => array( 4,3,5 )
-); 
+  /******************************************************************************************************
+   *  Sets an array corresponding to the number and widths of your footers from left to right. 
+   *  You can have as many footers as you want but the widths MUST add up to 12 or Foundation columns 
+   *  will hate you and stop answering your phone calls.
+   * 
+   *  Example:  3 columns of equal width  array(4,4,4);
+                2 columns of widths 4 and 8 array(4,8);
+   *****************************************************************************************************/
+    
+    'footers' => array( 4,3,5 )
+  ); 
+}
 update_option( 'cogito_init', $cogito_init );
 
 

45402850a84974b47cdf67450c71aa31a9177ac0
Matt <matt@raisedeyebrow.com>
2012-03-05 15:00:37 -0800

    adding a filter for the posted-on function

--- functions.php
+++ functions.php
@@ -66,15 +66,30 @@ if ( ! function_exists( 'cogito_posted_on' ) ) :
  * @since Twenty Eleven 1.0
  */
 function cogito_posted_on() {
-	printf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'cogito_wp' ),
-		esc_url( get_permalink() ),
-		esc_attr( get_the_time() ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_attr( sprintf( __( 'View all posts by %s', 'cogito_wp' ), get_the_author() ) ),
-		get_the_author()
+
+  $data = array(
+    'permalink' => esc_url( get_permalink() ),
+    'time' => esc_attr( get_the_time() ),
+    'date_year' => esc_attr( get_the_date( 'c' ) ),
+    'date' => esc_attr( get_the_date() ),
+    'author_url' => esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+    'all_posts_by' => esc_attr( sprintf( __( 'View all posts by %s', 'cogito_wp' ), get_the_author() ) ),
+    'author' => get_the_author()
+  );
+  
+	$data['output'] = sprintf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'cogito_wp' ),
+		$data['permalink'],
+		$data['time'],
+		$data['date_year'],
+		$data['date'],
+		$data['author_url'],
+		$data['all_posts_by'],
+		$data['author']
 	);
+	
+	$data = apply_filters('cogito_posted_on', $data );
+	
+	print $data['output'];
 }
 endif;
 

aa905a59f4440ff3b4e86408c5e2ac251b9fbd11
Matt Reimer <matt@raisedeyebrow.com>
2012-03-27 10:20:27 -0700

    Reworking the header block a little and using the new function to find logo.png

--- header_block.php
+++ header_block.php
@@ -1,24 +1,41 @@
-	 <div class="eleven columns centered"><?php //This just gives a little room on the edges.?>
-  	 <div class="row">
-    	 <div class="columns eight">
-             
-             <div class="skiplink"><a href="#main">Skip to main content? &darr;</a></div>    	 
-             
-    	     	 <?php // TITLE / LOGO and DESCRIPTION  ?>
-    				<h1 id="site-title">
-      				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-      				  <?php bloginfo( 'name' ); ?>
-      				</a>
-    				</h1>
-    				<h4 id="site-description" class="subheader"><?php bloginfo( 'description' ); ?></h4>
-    	 </div>
+<?php
+/************************************************************
+  Customizing your header:
 
-    	 <div class="columns four">
-    	 
-    	     	 <?php // SEARCH FORM  ?>
-    	     	 
-    	  <?php get_search_form(); ?>
-    	 </div>
-  		</div><?php // row  ?>
 
-   </div><?php //This just gives a little room on the edges.?>
+  (more documentation coming soon)
+  logo.png : Put a logo.png file into the root of your child them
+  or in the /images folder and it will get picked up by cogito_get_logo
+  
+  Use foundation classes to set what goes inside the <Header></header> tags
+
+*************************************************************/
+?>
+  <div class="row">
+    <?php if ( $logo = cogito_get_logo() ) : //Print a logo.png if there is one?>
+    <div class="columns two">
+      <img src="<?php print $logo; ?>">
+    </div>
+    <?php endif;?>
+    <div class="columns seven">
+    
+      <div class="skiplink"><a href="#main">Skip to main content? &darr;</a></div>    	 
+      
+      <?php // TITLE / LOGO and DESCRIPTION  ?>
+      <h1 id="site-title">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+          <?php bloginfo( 'name' ); ?>
+        </a>
+      </h1>
+      <h4 id="site-description" class="subheader"><?php bloginfo( 'description' ); ?></h4>
+      
+    </div>
+    
+    <div class="columns three">
+    
+      <?php // SEARCH FORM  ?>
+      <?php get_search_form(); ?>
+      
+    </div>
+  
+  </div><?php // row  ?>

9dd410a95f34b09459a6e2217132565b096a6931
Matt Reimer <matt@raisedeyebrow.com>
2012-04-09 14:49:59 -0700

    Adding better bullet support (really tiny fix) and adding ability for single posts to be more….. individual

--- loop.php
+++ loop.php
@@ -4,6 +4,58 @@
  *
  */
 ?>
+<?php if (is_single() ) : ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); sticky_class(); ?>>
+
+  <?php cogito_action_loop_item_top(); ?>
+
+	<header class="entry-header">
+  <?php $post_title_tag = is_single() ? "h1" : "h2"; ?>
+		<<? print $post_title_tag ?> class="entry-title"><?php the_title(); ?></<? print $post_title_tag ?>>
+		<?php cogito_posted_on(); ?>
+	</header><?php // .entry-header  ?>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'cogito_wp' ) . '</span>', 'after' => '</div>' ) ); ?>
+	</div><?php // .entry-content  ?>
+
+	<footer class="entry-meta">
+
+			<?php $show_sep = false; ?>
+			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ', ', 'cogito_wp' ) );
+				if ( $categories_list ):
+			?>
+			<span class="cat-links">
+				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'cogito_wp' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
+				$show_sep = true; ?>
+			</span>
+			<?php endif; // End if categories ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'cogito_wp' ) );
+				if ( $tags_list ):
+				if ( $show_sep ) : ?>
+			<span class="sep"> | </span>
+				<?php endif; // End if $show_sep ?>
+			<span class="tag-links">
+				<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'cogito_wp' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
+				$show_sep = true; ?>
+			</span>
+			<?php endif; // End if $tags_list ?>
+			<?php endif; // End if 'post' == get_post_type() ?>
+
+	 </footer><?php // .entry-meta  ?>
+
+    <?php cogito_action_loop_item_bottom(); ?>
+
+</article><!-- #post-<?php the_ID(); ?> -->
+
+<?php else : ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); sticky_class(); ?>>
 
@@ -53,3 +105,5 @@
     <?php cogito_action_loop_item_bottom(); ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
+
+<?php endif;?>

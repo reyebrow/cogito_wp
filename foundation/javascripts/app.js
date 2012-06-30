4cc51860139c422b6c07c11a419a2d7104a76c2c
Matt <matt.reimer@gmail.com>
2012-06-30 09:48:34 -0700

    the raw foundation files

--- foundation/javascripts/app.js
+++ foundation/javascripts/app.js
@@ -1,80 +1,117 @@
-/* Foundation v2.1.5 http://foundation.zurb.com */
-jQuery(document).ready(function () {
+jQuery(document).ready(function ($) {
 
-	/* Use this js doc for all application specific JS */
+  /* Use this js doc for all application specific JS */
 
-	/* TABS --------------------------------- */
-	/* Remove if you don't need :) */
+  /* TABS --------------------------------- */
+  /* Remove if you don't need :) */
 
-	function activateTab($tab) {
-		var $activeTab = $tab.closest('dl').find('a.active'),
-				contentLocation = $tab.attr("href") + 'Tab';
+  function activateTab($tab) {
+    var $activeTab = $tab.closest('dl').find('dd.active'),
+        contentLocation = $tab.children('a').attr("href") + 'Tab';
 
-		//Make Tab Active
-		$activeTab.removeClass('active');
-		$tab.addClass('active');
+    // Strip off the current url that IE adds
+    contentLocation = contentLocation.replace(/^.+#/, '#');
+
+    // Strip off the current url that IE adds
+    contentLocation = contentLocation.replace(/^.+#/, '#');
+
+    //Make Tab Active
+    $activeTab.removeClass('active');
+    $tab.addClass('active');
 
     //Show Tab Content
-		jQuery(contentLocation).closest('.tabs-content').children('li').hide();
-		jQuery(contentLocation).show();
+    $(contentLocation).closest('.tabs-content').children('li').hide();
+    $(contentLocation).css('display', 'block');
+  }
 
-	}
+  $('dl.tabs dd a').on('click.fndtn', function (event) {
+    activateTab($(this).parent('dd'));
+  });
 
-	jQuery('dl.tabs').each(function () {
-		//Get all tabs
-		var tabs = jQuery(this).children('dd').children('a');
-		tabs.click(function (e) {
-			activateTab(jQuery(this));
-		});
-	});
+  if (window.location.hash) {
+    activateTab($('a[href="' + window.location.hash + '"]'));
+    $.foundation.customForms.appendCustomMarkup();
+  }
 
-	if (window.location.hash) {
-		activateTab(jQuery('a[href="' + window.location.hash + '"]'));
-	}
-
-	/* ALERT BOXES ------------ */
-	jQuery(".alert-box").delegate("a.close", "click", function(event) {
+  /* ALERT BOXES ------------ */
+  $(".alert-box").delegate("a.close", "click", function(event) {
     event.preventDefault();
-	  jQuery(this).closest(".alert-box").fadeOut(function(event){
-	    jQuery(this).remove();
-	  });
-	});
+    $(this).closest(".alert-box").fadeOut(function(event){
+      $(this).remove();
+    });
+  });
+
+  /* PLACEHOLDER FOR FORMS ------------- */
+  /* Remove this and jquery.placeholder.min.js if you don't need :) */
+  $('input, textarea').placeholder();
+
+  /* TOOLTIPS ------------ */
+  $(this).tooltips();
+
+  /* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
+  //  $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+  //  $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+  //  $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+  //  $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
 
 
-	/* PLACEHOLDER FOR FORMS ------------- */
-	/* Remove this and jquery.placeholder.min.js if you don't need :) */
+  /* DROPDOWN NAV ------------- */
 
-	jQuery('input, textarea').placeholder();
+  var lockNavBar = false;
+  /* Windows Phone, sadly, does not register touch events :( */
+  if (Modernizr.touch || navigator.userAgent.match(/Windows Phone/i)) {
+    $('.nav-bar a.flyout-toggle').on('click.fndtn touchstart.fndtn', function(e) {
+      e.preventDefault();
+      var flyout = $(this).siblings('.flyout').first();
+      if (lockNavBar === false) {
+        $('.nav-bar .flyout').not(flyout).slideUp(500);
+        flyout.slideToggle(500, function(){
+          lockNavBar = false;
+        });
+      }
+      lockNavBar = true;
+    });
+    $('.nav-bar>li.has-flyout').addClass('is-touch');
+  } else {
+    $('.nav-bar>li.has-flyout').hover(function() {
+      $(this).children('.flyout').show();
+    }, function() {
+      $(this).children('.flyout').hide();
+    });
+  }
 
+  /* DISABLED BUTTONS ------------- */
+  /* Gives elements with a class of 'disabled' a return: false; */
 
+  /* SPLIT BUTTONS/DROPDOWNS */
+  $('.button.dropdown > ul').addClass('no-hover');
 
-	/* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
-//	jQuery('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
-//	jQuery('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
-//	jQuery('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
-//	jQuery('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+  $('.button.dropdown').on('click.fndtn touchstart.fndtn', function (e) {
+    e.stopPropagation();
+  });
+  $('.button.dropdown.split span').on('click.fndtn touchstart.fndtn', function (e) {
+    e.preventDefault();
+    $('.button.dropdown').not($(this).parent()).children('ul').removeClass('show-dropdown');
+    $(this).siblings('ul').toggleClass('show-dropdown');
+  });
+  $('.button.dropdown').not('.split').on('click.fndtn touchstart.fndtn', function (e) {
+    e.preventDefault();
+    $('.button.dropdown').not(this).children('ul').removeClass('show-dropdown');
+    $(this).children('ul').toggleClass('show-dropdown');
+  });
+  $('body, html').on('click.fndtn touchstart.fndtn', function () {
+    $('.button.dropdown ul').removeClass('show-dropdown');
+  });
 
+  // Positioning the Flyout List
+  var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)').outerHeight() - 1,
+      largeButtonHeight   = $('.button.large.dropdown').outerHeight() - 1,
+      smallButtonHeight   = $('.button.small.dropdown').outerHeight() - 1,
+      tinyButtonHeight    = $('.button.tiny.dropdown').outerHeight() - 1;
 
-
-	/* DROPDOWN NAV ------------- */
-
-	var lockNavBar = false;
-	jQuery('.nav-bar a.flyout-toggle').live('click', function(e) {
-		e.preventDefault();
-		var flyout = jQuery(this).siblings('.flyout');
-		if (lockNavBar === false) {
-			jQuery('.nav-bar .flyout').not(flyout).slideUp(500);
-			flyout.slideToggle(500, function(){
-				lockNavBar = false;
-			});
-		}
-		lockNavBar = true;
-	});
-
-
-
-	/* DISABLED BUTTONS ------------- */
-	/* Gives elements with a class of 'disabled' a return: false; */
-  
+  $('.button.dropdown:not(.large):not(.small):not(.tiny) > ul').css('top', normalButtonHeight);
+  $('.button.dropdown.large > ul').css('top', largeButtonHeight);
+  $('.button.dropdown.small > ul').css('top', smallButtonHeight);
+  $('.button.dropdown.tiny > ul').css('top', tinyButtonHeight);
 
 });

a30c74a8d520b0b52d9a5ee51d6908904d5c076e
Christine Rondeau <crondeau@raisedeyebrow.com>
2013-07-30 14:27:18 -0700

    Adding footer setting to child theme functions.php

--- STARTER_CHILD/functions.php
+++ STARTER_CHILD/functions.php
@@ -24,6 +24,18 @@ ALL SECTIONS MUST ADD UP TO total_columns (or not. maybe you like the look of a 
     'two_columns_lsb_content' => 8,
     
     //1 Column. Centered by default
-    'one_column_content'      => 10
+    'one_column_content'      => 10,
+
+  /******************************************************************************************************
+   *  Sets an array corresponding to the number and widths of your footers from left to right. 
+   *  You can have as many footers as you want but the widths MUST add up to total_columns or Foundation columns 
+   *  will hate you and stop answering your phone calls.
+   * 
+   *  Example:  3 columns of equal width  array(4,4,4);
+                2 columns of widths 4 and 8 array(4,8);
+   *****************************************************************************************************/
+
+    'footers' => array( 4,3,5 )
+
   );  
   update_option( 'cogito_columns', $cogito_init );

3c3ec81a57c80eca76fdaec00e98f6aa1e462432
Christine Rondeau <crondeau@raisedeyebrow.com>
2013-07-30 15:13:10 -0700

    Simplified the loop. There's no need for if else or pagination. It's just a page.

--- page.php
+++ page.php
@@ -8,26 +8,18 @@
 
 get_header(); ?>
 
-
     <?php // Main Content ?>
     <div id="content" class="<?php print cogito_wp_col_class( 'content' ); ?>" role="main">
   
       <?php cogito_content_top(); cogito_content_top_page(); ?>
 
-      <?php if ( have_posts() ) {
-        while ( have_posts() ) {
+      <?php while ( have_posts() ) : the_post(); ?>
+
+          <?php get_template_part( 'loop', 'page' ); ?>
           
-          the_post();  //set up $post variable
-          get_template_part( 'loop', 'page' ); //basically this is just looking for loop-format.php 
-          
-          cogito_wp_content_nav( 'nav-below' ); 
-        }
-      }
-      else {
-        get_template_part( 'loop','noresult' );
-      } ?>   
-      
-      <?php if (function_exists( 'emm_paginate' )) { emm_paginate(); } ?>
+          <?php cogito_wp_content_nav( 'nav-below' ); ?>
+       
+      <?php endwhile;   // end of the loop. ?>
     
       <?php cogito_content_bottom(); cogito_content_bottom_page(); ?>
     
